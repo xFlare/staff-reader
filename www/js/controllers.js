4188254d5c9f82fb3896930288bbe0653bec7c62
Ricardo Augustine <augustineri93@gmail.com>
2016-01-17 21:52:14 -0400

    added vexflow

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,46 @@ angular.module('starter.controllers',[])
 
 .controller('HomeCtrl', ['$scope', function($scope){
     
+   function getParameter(name) {
+      name = name.replace(/\[]/, "\\\[").replace(/[\]]/, "\\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+      var results = regex.exec(window.location.search);
+      if (results == null) return undefined;
+      else return decodeURIComponent(results[1].replace(/\+/g, " "));
+    } 
+    
+    var VexDocument = null;
+    var VexFormatter = null;
+    
+    $(document).ready(function() {
+      var req = new XMLHttpRequest();
+      var uri = getParameter('doc');
+      console.log(uri);
+      if (! uri) uri = 'assets/bach_bwv846p.xml';
+      req.open('GET', uri, true);
+      req.onreadystatechange = function() {
+      if (req.readyState != 4) return;
+    //   VexDocument = new Vex.Flow.Document(req.responseText);
+    //   VexFormatter = doc.getFormatter();
+    //   VexFormatter.setWidth(800).draw($("#viewer")[0]);
+       
+       try {
+          VexDocument = new Vex.Flow.Document(req.responseText);
+        }
+        catch (e) {
+          $(".content")[0].innerHTML = "There was an error with the document.";
+          console.log(e);
+        }
+        var content = $(".content")[0];
+        if (VexDocument) {
+          VexFormatter = VexDocument.getFormatter();
+          VexFormatter.draw(content);
+        }
+        
+      };
+      req.send(null);
+    });
+    
 }])
 
 .controller('SettingCtrl', ['$scope', function($scope){
