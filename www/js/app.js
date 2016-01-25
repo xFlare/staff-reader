c4a855543333321176575e22c2e2a5858cd82bac
Ricardo Augustine <augustineri93@gmail.com>
2016-01-25 08:49:19 -0400

    added musicjson

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+require('musicjson');
+
 var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 app.run(function($ionicPlatform) {
@@ -22,6 +24,8 @@ app.run(function($ionicPlatform) {
     }
   });
 })
+
+
 
 app.config(function($stateProvider, $urlRouterProvider){
     $stateProvider.state('home',{
